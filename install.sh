ce8f8161f6dcb7bab94a332ea4b579a1462fe556
Jack Zhang <954294627@qq.com>
2022-06-26 23:32:38 +0800

    修复文件夹名称被分割的问题，修复换行符问题。

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@
 cd ~
 mkdir -p tmp
 cd tmp
-wget https://download.fastgit.org//zzxzzk115/BBKGames_GameShell/releases/latest/download/BBK_Games.zip
-unzip BBK_Games.zip
+wget https://download.fastgit.org//zzxzzk115/BBKGames_GameShell/releases/latest/download/BBK-Games.zip
+unzip BBK-Games.zip
 
 # 安装
-cd ~/tmp/BBK_Games
+cd ~/tmp/BBK-Games
 sudo chmod +x install.sh
 bash install.sh
 
 # 删除临时内容
-sudo rm -rf ~/tmp/BBK_Games*
+sudo rm -rf ~/tmp/BBK-Games*
 
 # 提示安装完成，需要刷新菜单界面
 echo Done! Please refresh menu.
